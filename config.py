c011fd87c6df1ff222f7cefce11595e86e300db1
Jan Derk <info@digitaldutch.com>
2022-08-02 21:07:38 +0200

    - Cleanups and small modifications

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import locale
 
-version = 23
+version = 25
 version_date = '2 augustus 2022'
 
 locale.setlocale(locale.LC_ALL, 'nl_NL')
@@ -17,7 +17,8 @@ file_db_sqlite = 'output/bag.sqlite'
 # log file with progress, warnings and error messages. This info is also written to the console
 file_log = 'output/bag_importer.log'
 
-# The parser creates an addresses table. After that some BAG tables are no longer needed: nummers, panden,
-# verblijfobjecten and ligplaatsen. You can also delete these afterwards using the utils_sqlite_shrink.py script.
+# The parser creates an 'adressen' table. After that some BAG tables are no longer needed and will be deleted:
+# nummers, panden, verblijfobjecten and ligplaatsen. Set to False if you want to keep these tables.
+# You can also delete these afterwards using the utils_sqlite_shrink.py script.
 delete_no_longer_needed_bag_tables = True
 
