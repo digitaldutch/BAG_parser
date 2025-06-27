6b36c604b34bc0f80b384e0cd560912906d09802
Remco Poortinga - van Wijnen <remco.poortinga@surf.nl>
2025-06-27 09:35:56 +0200

    Switch default to Parquet, added option for exporting geometry

--- export.py
+++ export.py
@@ -3,11 +3,14 @@ from argparse import ArgumentParser
 
 from exporter import Exporter
 
-parser = ArgumentParser(description='Export addresses in DuckDB database to a CSV, Parquet or JSON file')
+parser = ArgumentParser(description='Export addresses in DuckDB database to a Parquet, CSV or JSON file')
 
 helpText = ("Export all data including year of construction, latitude, longitude, floor area and intended use of "
             "buildings")
 parser.add_argument('-a', '--all', action='store_true', help=helpText)
+
+helpText = ("Export all data as above, but including geometry as well (if generated)")
+parser.add_argument('-ag', '--geometry', action='store_true', help=helpText)
 
 helpText = "Export statistics of 4 character postal code groups (e.g. 1000)"
 parser.add_argument('-p4', '--postcode4', action='store_true', help=helpText)
@@ -18,27 +21,29 @@ parser.add_argument('-p5', '--postcode5', action='store_true', help=helpText)
 helpText = "Export statistics of 6 character postal code groups (e.g. 1000AA)"
 parser.add_argument('-p6', '--postcode6', action='store_true', help=helpText)
 
-helpText = "Export to parquet rather than CSV"
-parser.add_argument('--parquet', action='store_true', help=helpText)
+helpText = "Export to CSV rather than Parquet"
+parser.add_argument('--csv', action='store_true', help=helpText)
 
-helpText = "Export to JSON rather than CSV"
+helpText = "Export to JSON rather than Parquet"
 parser.add_argument('--json', action='store_true', help=helpText)
 
 args = parser.parse_args()
 
 exporter = Exporter()
 
-ext = 'csv'
-export_options = "(HEADER, DELIMITER ',')"
-if args.parquet:
-    ext = 'parquet'
-    export_options = "(FORMAT parquet)"
+ext = 'parquet'
+export_options = "(FORMAT parquet)"
+if args.csv:
+    ext = 'csv'
+    export_options = "(HEADER, DELIMITER ',')"
 elif args.json:
     ext = 'json'
     export_options = "(ARRAY)"
 
 if args.all:
     exporter.export(f'output/adressen_all_data.{ext}', export_options)
+elif args.geometry:
+    exporter.export(f'output/adressen_all_data_geometry.{ext}', export_options, True)
 elif args.postcode4:
     exporter.export_postcode4_stats(f'output/adressen_p4_stats.{ext}', export_options)
 elif args.postcode5:
